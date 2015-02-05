11139c7380fad2ae55d74f3c3119fc8d2917750d
Dean Marano <dmarano@groupon.com>
2015-02-04 22:18:00 -0600

    Add grpn.sh to git.

---
+++ grpn.sh
@@ -0,0 +1,6 @@
+cdg() { cd ~/groupon/; if [ $# -eq 1 ]; then cd $1; fi }
+alias cdh="cd ~/groupon/hermes"
+alias cds="cd ~/groupon/spinderella"
+alias cda="cd ~/groupon/accounting-service"
+alias cdb="cd ~/groupon/bling-3"
+
