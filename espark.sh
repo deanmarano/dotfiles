e1f2d2b27067703c23b0dcc6b9a9193117ba999f
Dean Marano <dean.marano@gmail.com>
2016-03-18 09:56:39 -0500

    eSpark additions.

--- espark.sh
+++ espark.sh
@@ -1,1 +1,3 @@
-alias cde="cd ~/github/espark"
+rebase() { git wip; git checkout master; git pull; git checkout $1; git pull --rebase origin master; git push -f; git checkout master; };
+cde() { cd ~/github/espark/; if [ $# -eq 1 ]; then cd $1; fi };
+alias prod-console="ssh console.esparklearning.com -t \"sudo -u deploy /home/dean/console.sh\""
