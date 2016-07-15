4972689f3b1d7a9d26489bee7590d44838276b72
Dean Marano <dean.marano@gmail.com>
2016-07-15 12:28:46 -0500

    Script to help cleanup local branches.

---
+++ bin/branch.rb
@@ -0,0 +1,8 @@
+require 'pry'
+
+branches = `git branch`
+branches.split.reject { |b| ['master', '*'].include? b }.each do |branch|
+  `git checkout #{branch}`
+  `git pull --rebase --ff-only origin master`
+  `git rebase --abort`
+end
