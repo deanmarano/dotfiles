38b93563dbc24fe0bfe60fbf95739b3978638516
Dean Marano <dean.marano@gmail.com>
2016-07-26 12:46:59 -0700

    Remove outdated backup file.

--- backup_dotfiles.sh
+++
@@ -1,4 +0,0 @@
-cp -r ~/.vim ~/Dropbox\ \(Personal\)/dotfiles
-cp ~/.vimrc ~/Dropbox\ \(Personal\)/dotfiles
-cp ~/.bashrc ~/Dropbox\ \(Personal\)/dotfiles
-cp ~/.tmux.conf ~/Dropbox\ \(Personal\)/dotfiles
