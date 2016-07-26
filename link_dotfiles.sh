91a45e7b2a8d72e8bc83c13b2ff94b6131e1573a
Dean Marano <dean.marano@gmail.com>
2016-07-26 12:46:46 -0700

    Don't use dot filename for vimrc in dotfiles folder.

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,3 +1,4 @@
+# Backup existing files if they exist
 mkdir -p ~/.original-dotfiles
 mv ~/.tmux.conf ~/.original-dotfiles/.tmux.conf 2> /dev/null
 mv ~/.vimrc ~/.original-dotfiles/.vimrc 2> /dev/null
@@ -9,6 +10,7 @@ mv ~/.bashrc ~/.original-dotfiles/.bashrc 2> /dev/null
 mv ~/.gemrc ~/.original-dotfiles/.gemrc 2> /dev/null
 
 
+# Git config is slightly different based on OS
 if [ "$(expr $(uname -s))" == "Darwin" ]; then
   ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
@@ -16,13 +18,14 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 fi
 
 mkdir -p ~/.config
+mkdir -p ~/bin
+mkdir -p ~/.cache/vim/{backup,swap,undo}
+
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.nvimrc
 ln -s ~/dotfiles/.vim ~/.nvim
-ln -s ~/dotfiles/.vimrc ~/nvim/init.vim
+ln -s ~/dotfiles/vimrc ~/nvim/init.vim
 ln -s ~/dotfiles/.gemrc ~/.gemrc
 
-mkdir -p ~/bin
