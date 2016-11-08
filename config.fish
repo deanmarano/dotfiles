ae2108b2ea1b8ed0c8b20edf3c9952d6f7a6e269
Dean Marano <dean.marano@gmail.com>
2016-11-07 22:24:43 -0600

    Add nvm support for fish.

--- config.fish
+++ config.fish
@@ -1,5 +1,8 @@
 set fish_greeting ""
 
+fundle plugin 'edc/bass'
+fundle init
+
 function cddm
   cd ~/github/deanmarano/
 end
@@ -13,4 +16,8 @@ end
 
 function localnet
   nmap -sn 192.168.1.0/24 -oG - | awk '/Up$/{print $2}'
+end
+
+function nvm
+  bass source ~/.linuxbrew/opt/nvm/nvm.sh --no-use ';' nvm $argv
 end
