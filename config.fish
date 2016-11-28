b5bf3025b8ccfd3cd7a8522535de9b104ccf7d3e
Dean Marano <dean.marano@gmail.com>
2016-11-27 18:57:41 -0600

    Alias vi to vim, easy open journaling.

--- config.fish
+++ config.fish
@@ -3,6 +3,8 @@ set fish_greeting ""
 fundle plugin 'edc/bass'
 fundle init
 
+alias vi="vim"
+
 function cddm
   cd ~/github/deanmarano/
 end
@@ -20,4 +22,12 @@ end
 
 function nvm
   bass source ~/.linuxbrew/opt/nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+function words
+  vim ~/Dropbox/Documents/journal/(date +%Y.%m.%d).md -c ':AutoSaveToggle'
+end
+
+function links
+  vim ~/Dropbox/Documents/links/(date +%Y.%m.%d).md -c ':AutoSaveToggle'
 end
