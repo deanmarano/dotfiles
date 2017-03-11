77ccdc0446f554a3ca7d3dce66d08b97b7ea3e24
Dean Marano <dean.marano@gmail.com>
2017-03-11 14:58:34 -0600

    Better formatting of tmux status line.

--- bin/battery.sh
+++ bin/battery.sh
@@ -2,7 +2,7 @@ charging=$(cat /sys/class/power_supply/AC/online)
 percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | sed 's/percentage://' | sed 's/ //g')
 
 if [ $charging -eq 1 ]; then
-  echo "🔌"$percentage
+  echo "🔌 "$percentage
 else
   echo " "$percentage
 fi
