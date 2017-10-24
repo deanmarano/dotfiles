4c523e9f7a3a94c132ca6bb74f5973fd1f4b50f6
Dean Marano <dean.marano@gmail.com>
2017-10-24 10:17:19 -0500

    Make battery.sh work on the mac, unify shell files.

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-charging=$(cat /sys/class/power_supply/AC/online)
-percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | sed 's/percentage://' | sed 's/ //g')
+charging=0
+percentage=''
+
+if [ "$(expr $(uname -s))" == "Darwin" ]; then
+  charging=$(pmset -g batt | grep 'AC Power' | wc -l | sed 's/ //g')
+  percentstring=($(pmset -g batt | tail -1))
+  percentage=$(echo ${percentstring[2]} | sed 's/;//')
+else
+  charging=$(cat /sys/class/power_supply/AC/online)
+  percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | sed 's/percentage://' | sed 's/ //g')
+fi
 
 if [ $charging -eq 1 ]; then
   echo "🔌 "$percentage
--- bin/mac-battery.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-ioreg -l | grep -i capacity | tr '\n' ' | ' | awk '{printf("%.2f%%\n", $10/$5 * 100)}'
