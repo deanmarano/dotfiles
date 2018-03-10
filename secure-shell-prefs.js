9d37a448d817e3110514d4e98360275e7dcaace3
Dean Marano <dean.marano@gmail.com>
2018-03-10 15:29:31 -0600

    Update and format secure shell preferences.

--- secure-shell-prefs.js
+++ secure-shell-prefs.js
@@ -1,1 +1,217 @@
-{"magic":"nassh-prefs","version":1,"nassh":{"profile-ids":[{"id":"f638","json":{"description":"dean@fusion.deanoftech.com","username":"dean","hostname":"fusion.deanoftech.com","port":5720,"argstr":"-L 4200/localhost/4200 -L 35729/localhost/35729 -L 8000/localhost/8000"}},{"id":"48fb","json":{"description":"dean@localhost","username":"dean","hostname":"localhost","port":""}},{"id":"9d8e","json":{"description":"MBP","username":"dean","hostname":"192.168.1.59","port":""}},{"id":"d723","json":{"description":"pi@192.168.1.105","username":"pi","hostname":"192.168.1.105","port":""}},{"id":"b192","json":{"description":"vm-1@digitalocean","username":"dean","hostname":"104.131.168.122","port":"","argstr":"-L 8888/localhost/8888 -L 3000/localhost/3000 -X"}},{"id":"7fd0","json":{"description":"dean@192.168.1.2","username":"dean","hostname":"192.168.1.2","port":""}},{"id":"886d","json":{"description":"dean@192.168.1.198","username":"dean","hostname":"192.168.1.119","port":""}},{"id":"75cc","json":{"description":"qxr9638@localhost","username":"qxr9638","hostname":"localhost"}}],"welcome/notes-version":289,"welcome/show-count":2},"hterm":{"default":{"audible-bell-sound":"","background-color":"#fdf6e3","cursor-blink":true,"cursor-blink-cycle":[1000,500],"cursor-color":"rgba(0, 43, 54, 0.44)","color-palette-overrides":["#eee8d5","#dc322f","#859900","#b58900","#268bd2",null,"#2aa198","#073642","#002b36","#cb4b16","#93a1a1","#839496","#657b83","#6c71c4","#586e75","#002b36"],"copy-on-select":false,"use-default-window-copy":true,"enable-bold":null,"environment":{"TERM":"screen-256color"},"font-family":"\"Source Code Pro\", monospace","foreground-color":"#657b83","user-css":"http://fonts.googleapis.com/css?family=Ubuntu+Mono|Droid+Sans+Mono|Source+Code+Pro|Anonymous+Pro"},"pixelbook":{"background-color":"#fdf6e3","cursor-color":"#002b36","color-palette-overrides":["#eee8d5","#dc322f","#859900","#b58900","#268bd2","#d33682","#2aa198","#073642","#fdf6e3","#cb4b16","#93a1a1","#839496","#657b83","#6c71c4","#586e75","#002b36"],"enable-bold":null,"foreground-color":"#657b83","terminal-encoding":"iso-2022"},"solarized-dark":{"background-color":"#002b36","cursor-color":"rgba(131, 148, 150, 0.5)","color-palette-overrides":["#073642","#dc322f","#859900","#b58900","#268bd2","#d33682","#2aa198","#eee8d5","#002b36","#cb4b16","#586e75","#657b83","#839496","#6c71c4","#93a1a1","#fdf6e3"],"enable-bold":false,"font-size":12,"font-smoothing":"subpixel-antialiased","foreground-color":"#839496"},"solarized-light":{"background-color":"#fdf6e3","cursor-color":"#002b36","color-palette-overrides":["#eee8d5","#dc322f","#859900","#b58900","#268bd2",null,"#2aa198","#073642","#002b36","#cb4b16","#93a1a1","#839496","#657b83","#6c71c4","#586e75","#002b36"],"enable-bold":true,"enable-bold-as-bright":false,"environment":{"TERM":"xterm-color"},"font-family":"\"Source Code Pro\", monospace","font-size":"15","font-smoothing":"subpixel-antialiased","foreground-color":"#657b83","user-css":"http://fonts.googleapis.com/css?family=Ubuntu+Mono|Droid+Sans+Mono|Source+Code+Pro|Anonymous+Pro"}}}
+{
+  "hterm": {
+    "default": {
+      "alt-is-meta": true,
+      "audible-bell-sound": "",
+      "background-color": "#fdf6e3",
+      "character-map-overrides": {
+        "168": "'\u001bOQ'",
+        "182": "'\u001bOS'",
+        "183": "'\u001bOR'",
+        "216": "'\u001b[15~'",
+        "217": "'\u001b[17~'"
+      },
+      "color-palette-overrides": [
+        "#eee8d5",
+        "#dc322f",
+        "#859900",
+        "#b58900",
+        "#268bd2",
+        null,
+        "#2aa198",
+        "#073642",
+        "#002b36",
+        "#cb4b16",
+        "#93a1a1",
+        "#839496",
+        "#657b83",
+        "#6c71c4",
+        "#586e75",
+        "#002b36"
+      ],
+      "copy-on-select": false,
+      "cursor-blink": true,
+      "cursor-blink-cycle": [
+        1000,
+        500
+      ],
+      "cursor-color": "rgba(201, 197, 197, 0.5)",
+      "enable-blink": false,
+      "enable-bold": null,
+      "environment": {
+        "TERM": "screen-256color"
+      },
+      "font-family": "\"SauceCodePro Nerd Font Mono\", monospace",
+      "font-size": "14",
+      "foreground-color": "#657b83",
+      "scroll-wheel-move-multiplier": "20",
+      "use-default-window-copy": true,
+      "user-css-text": "@font-face {\n  font-family: 'SauceCodePro Nerd Font';\n     url('https://rawgit.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce Code Pro Nerd Font Complete.ttf')  format('truetype'),\n}"
+    },
+    "pixelbook": {
+      "background-color": "#fdf6e3",
+      "color-palette-overrides": [
+        "#eee8d5",
+        "#dc322f",
+        "#859900",
+        "#b58900",
+        "#268bd2",
+        "#d33682",
+        "#2aa198",
+        "#073642",
+        "#fdf6e3",
+        "#cb4b16",
+        "#93a1a1",
+        "#839496",
+        "#657b83",
+        "#6c71c4",
+        "#586e75",
+        "#002b36"
+      ],
+      "cursor-color": "#002b36",
+      "enable-bold": null,
+      "foreground-color": "#657b83",
+      "terminal-encoding": "iso-2022"
+    },
+    "solarized-dark": {
+      "background-color": "#002b36",
+      "color-palette-overrides": [
+        "#073642",
+        "#dc322f",
+        "#859900",
+        "#b58900",
+        "#268bd2",
+        "#d33682",
+        "#2aa198",
+        "#eee8d5",
+        "#002b36",
+        "#cb4b16",
+        "#586e75",
+        "#657b83",
+        "#839496",
+        "#6c71c4",
+        "#93a1a1",
+        "#fdf6e3"
+      ],
+      "cursor-color": "rgba(131, 148, 150, 0.5)",
+      "enable-bold": false,
+      "font-size": 12,
+      "font-smoothing": "subpixel-antialiased",
+      "foreground-color": "#839496"
+    },
+    "solarized-light": {
+      "background-color": "#fdf6e3",
+      "color-palette-overrides": [
+        "#eee8d5",
+        "#dc322f",
+        "#859900",
+        "#b58900",
+        "#268bd2",
+        null,
+        "#2aa198",
+        "#073642",
+        "#002b36",
+        "#cb4b16",
+        "#93a1a1",
+        "#839496",
+        "#657b83",
+        "#6c71c4",
+        "#586e75",
+        "#002b36"
+      ],
+      "cursor-color": "#002b36",
+      "enable-bold": true,
+      "enable-bold-as-bright": false,
+      "environment": {
+        "TERM": "xterm-color"
+      },
+      "font-family": "\"Source Code Pro\", monospace",
+      "font-size": "15",
+      "font-smoothing": "subpixel-antialiased",
+      "foreground-color": "#657b83",
+      "user-css": "http://fonts.googleapis.com/css?family=Ubuntu+Mono|Droid+Sans+Mono|Source+Code+Pro|Anonymous+Pro"
+    }
+  },
+  "magic": "nassh-prefs",
+  "nassh": {
+    "profile-ids": [
+      {
+        "id": "f638",
+        "json": {
+          "argstr": "-L 4200/localhost/4200 -L 35729/localhost/35729 -L 8000/localhost/8000",
+          "description": "dean@fusion.deanoftech.com",
+          "hostname": "fusion.deanoftech.com",
+          "port": 5720,
+          "username": "dean"
+        }
+      },
+      {
+        "id": "48fb",
+        "json": {
+          "description": "dean@localhost",
+          "hostname": "localhost",
+          "port": "",
+          "username": "dean"
+        }
+      },
+      {
+        "id": "9d8e",
+        "json": {
+          "description": "MBP",
+          "hostname": "192.168.1.59",
+          "port": "",
+          "username": "dean"
+        }
+      },
+      {
+        "id": "d723",
+        "json": {
+          "description": "pi@192.168.1.105",
+          "hostname": "192.168.1.105",
+          "port": "",
+          "username": "pi"
+        }
+      },
+      {
+        "id": "b192",
+        "json": {
+          "argstr": "-L 8888/localhost/8888 -L 3000/localhost/3000 -X",
+          "description": "vm-1@digitalocean",
+          "hostname": "104.131.168.122",
+          "port": "",
+          "username": "dean"
+        }
+      },
+      {
+        "id": "7fd0",
+        "json": {
+          "description": "dean@192.168.1.2",
+          "hostname": "192.168.1.2",
+          "mount-path": "/mnt/eta/",
+          "port": "",
+          "username": "dean"
+        }
+      },
+      {
+        "id": "886d",
+        "json": {
+          "description": "dean@192.168.1.198",
+          "hostname": "192.168.1.119",
+          "port": "",
+          "username": "dean"
+        }
+      },
+      {
+        "id": "75cc",
+        "json": {
+          "description": "qxr9638@localhost",
+          "hostname": "localhost",
+          "username": "qxr9638"
+        }
+      }
+    ],
+    "welcome/notes-version": 555,
+    "welcome/show-count": 10
+  },
+  "version": 1
+}
