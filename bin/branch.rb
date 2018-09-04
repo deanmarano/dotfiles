6b28f3ae86da095406e6dedd65a3bda81ff6313e
Dean Marano <dean.marano@gmail.com>
2018-09-04 10:32:40 -0500

    Improvements to branch cleanup script.

--- bin/branch.rb
+++ bin/branch.rb
@@ -6,3 +6,5 @@ branches.split.reject { |b| ['master', '*'].include? b }.each do |branch|
   `git pull --rebase --ff-only origin master`
   `git rebase --abort`
 end
+`git checkout master`
+`git merged`
