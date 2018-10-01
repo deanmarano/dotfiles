1ef10378d26912ae520b7d269efd0c2f89c38efc
Dean Marano <dean.marano@gmail.com>
2018-10-01 11:57:56 -0500

    No history for fish for screencasts.

--- config.fish
+++ config.fish
@@ -61,3 +61,19 @@ abbr -a migrate bin/rake db:migrate
 
 status --is-interactive; and source (rbenv init -|psub)
 
+# https://github.com/fish-shell/fish-shell/issues/1363
+function nohist
+    set -l prefix $HOME/.config
+    set -l path fish/fish_history
+
+    if set -q XDG_CONFIG_HOME
+        set prefix $XDG_CONFIG_HOME
+    end
+
+    if test -f $prefix/$path.copy
+        mv $prefix/$path.copy $prefix/$path
+    else
+        cp -f $prefix/$path $prefix/$path.copy
+        echo y | history --clear > /dev/null
+    end
+end
