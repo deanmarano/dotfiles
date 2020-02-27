8e3d0a6a1b25f5c0da066d2ca65ce8adec0b83b1
Dean Marano <dean.marano@gmail.com>
2020-02-26 21:01:35 -0600

    Add script for setting ssh key permissions.

---
+++ bin/ssh-key-permissions.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Source: https://gist.github.com/grenade/6318301
+# URL: https://gist.githubusercontent.com/grenade/6318301/raw/8626132fb176338f045053cfe695da5782a3347d/ssh-key-permissions.sh
+chmod 700 ~/.ssh
+chmod 644 ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/known_hosts
+chmod 644 ~/.ssh/config
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/github_rsa
+chmod 644 ~/.ssh/github_rsa.pub
+chmod 600 ~/.ssh/mozilla_rsa
+chmod 644 ~/.ssh/mozilla_rsa.pub
