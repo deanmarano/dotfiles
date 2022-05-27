95acf1f41a15a671cb298a4911325f159e41d6a6
Dean Marano <dean.marano@gmail.com>
2022-05-27 11:43:35 -0400

    Cleanup remove nvim and emacs config, add tool-versions (asdf) and zsh.

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -3,12 +3,9 @@ mkdir -p /tmp/original-dotfiles
 mv ~/.tmux.conf /tmp/original-dotfiles/.tmux.conf 2> /dev/null
 mv ~/.vimrc /tmp/original-dotfiles/.vimrc 2> /dev/null
 mv ~/.vim /tmp/original-dotfiles/.vim 2> /dev/null
-mv ~/.nvim /tmp/original-dotfiles/.nvim 2> /dev/null
-mv ~/.nvimrc /tmp/original-dotfiles/.vimrc 2> /dev/null
 mv ~/.gitconfig /tmp/original-dotfiles/.gitconfig 2> /dev/null
 mv ~/.bashrc /tmp/original-dotfiles/.bashrc 2> /dev/null
 mv ~/.gemrc /tmp/original-dotfiles/.gemrc 2> /dev/null
-
 
 # Git config is slightly different based on OS
 if [ "$(expr $(uname -s))" == "Darwin" ]; then
@@ -20,22 +17,22 @@ ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 
 mkdir -p ~/.cache/swap/{backup,undo,swap}
 mkdir -p ~/{bin,.config,.ssh}
-mkdir -p ~/.config/fish
-mkdir -p ~/.config/fish/functions
 
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/ssh.config ~/.ssh/config
+ln -s ~/dotfiles/tool-versions ~/.tool-versions
 ln -s ~/dotfiles/.gemrc ~/.gemrc
 ln -s ~/dotfiles/.npmrc ~/.npmrc
-
 
 # Vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vim ~/.nvim
 
 # Fish
+mkdir -p ~/.config/fish
+mkdir -p ~/.config/fish/functions
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
 ln -s ~/dotfiles/fishfile ~/.config/fish/fishfile
 ln -s ~/dotfiles/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
@@ -45,6 +42,3 @@ mkdir -p ~/.config/kitty
 ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 
 ln -s ~/dotfiles/.asdfrc ~/.asdfrc
-ln -s ~/dotfiles/.emacs ~/.emacs
-ln -s ~/dotfiles/.spacemacs ~/.spacemacs
-ln -s ~/dotfiles/.emacs.d ~/.emacs.d
---
+++ zshrc
@@ -0,0 +1,2 @@
+eval `ssh-agent -s` > /dev/null
+export PATH="/opt/homebrew/bin:$PATH"
