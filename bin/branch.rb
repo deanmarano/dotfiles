07f549890ba042ee49227274121c3064d40c7a91
Dean Marano <here@dean.is>
2022-09-02 21:58:38 -0400

    Switch to main branch name in branch.rb.

--- bin/branch.rb
+++ bin/branch.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 branches = `git branch`
-branches.split.reject { |b| ['master', '*'].include? b }.each do |branch|
+branches.split.reject { |b| ['main', 'trunk', '*'].include? b }.each do |branch|
   `git checkout #{branch}`
-  `git pull --rebase --ff-only origin master`
+  `git pull --rebase --ff-only origin main`
   `git rebase --abort`
 end
-`git checkout master`
+`git checkout main`
 `git merged`
