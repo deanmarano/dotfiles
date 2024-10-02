8704687e5157151eaa766b44b576493888920757
Dean Marano <dean.marano@hashicorp.com>
2024-10-02 15:39:54 -0400

    Update branch script to output more.

--- bin/branch.rb
+++ bin/branch.rb
@@ -3,8 +3,8 @@ require 'pry'
 branches = `git branch`
 branches.split.reject { |b| ['main', 'trunk', '*'].include? b }.each do |branch|
   `git checkout #{branch}`
-  `git pull --rebase --ff-only origin main`
+  `git pull --rebase origin main`
   `git rebase --abort`
 end
 `git checkout main`
-`git merged`
+puts `git merged`
